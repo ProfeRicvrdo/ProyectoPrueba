5e94ad870aae60c1eb11589292342383f9e86aad
ProfeRicvrdo <ri.yaneza@profesor.duoc.cl>
2024-06-03 17:33:46 -0400

    crecion de variables

--- divi.py
+++ divi.py
@@ -1,8 +1,9 @@
 try:
+    numerador =0;
+    denominador =0;
     resultado = numerador / denominador
 except ZeroDivisionError as e:
     print(f"Error: No se puede dividir por cero.")
     
 else:
     print (resultado)
-
